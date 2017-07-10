0e780441e7d12e886a826d37382350e8ba593929
TesLA_422 <tesla422zxc@outlook.com>
2017-07-10 13:29:42 +0800

    Update 11.ContainerWithMostWater.cpp

--- Algorithm/cpp/11.ContainerWithMostWater.cpp
+++ Algorithm/cpp/11.ContainerWithMostWater.cpp
@@ -1,6 +1,6 @@
 /*
  * 11.Container With Most Water
- * Given n non-negatice intergers a1,a2,...,an,where each represents a point at coordinate(i,ai).n vertical inles are drawn such that the
+ * Given n non-negatice integers a1,a2,...,an,where each represents a point at coordinate(i,ai).n vertical inles are drawn such that the
  * two endpoints of line i is at (i,ai) and (i,0).Find two lines,which together with x-axis forms a container,such that the container contains 
  * the most water.
  * Note:You may not slant the container and n is at least 2.

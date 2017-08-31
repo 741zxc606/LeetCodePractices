60537c714b7cf2d00c93d7c6aa062e64b4ff809c
TesLA_422 <tesla422zxc@gmail.com>
2017-08-31 14:49:41 +0800

    Update 24.cpp

--- Algorithm/cpp/24.cpp
+++ Algorithm/cpp/24.cpp
@@ -25,24 +25,21 @@ class Solution
 public:
     ListNode* swapPairs(ListNode* head)
     {
-        if (head == NULL || head->next == NULL) return head;
+        ListNode dummy{ 0 };
+        dummy.next = head;
+        auto curr = &dummy;
+
+        while (curr->next && curr->next->next) 
         {
-            ListNode *helper = new ListNode(0);
-            ListNode *ret = head;
-            ListNode *cur = helper;
-            while (ret&&ret->next)
-            {
-                ListNode *next = ret->next->next;
-                cur->next = ret->next;
-                cur = cur->next;
-                cur->next = ret;
-                cur = cur->next;
-                cur->next == NULL;
-                ret = next;
-            }
-            if (ret) cur->next = ret;
-            return helper->next;
+            auto next_one = curr->next;
+            auto next_two = next_one->next;
+            auto next_three = next_two->next;
+            curr->next = next_two;
+            next_two->next = next_one;
+            next_one->next = next_three;
+            curr = next_one;
         }
+        return dummy.next;
     }
 };
 

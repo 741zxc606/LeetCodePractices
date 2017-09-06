69de3778ca79f98cc43b4eb9657151b3d5acd706
TesLA_422 <tesla422zxc@gmail.com>
2017-09-06 13:27:22 +0800

    Create 33.SearchinRotatedSortedArray.cpp

---
+++ Algorithm/cpp/33.SearchinRotatedSortedArray.cpp
@@ -0,0 +1,74 @@
+/*
+* 33.Search in Rotated Sorted Array
+* Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+* (i.e.,0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+* You are given a target value to search.If found in the array return its index,otherwise return -1.
+* You may assume no duplicate exists in the array.
+*/
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+private:
+    int find(vector<int> &A, int l, int r, int target)
+    {
+        if (l > r)
+        {
+            return -1;
+        }
+        int idx = -1;
+        if (A[l] <= A[r])
+        {
+            int left = l, right = r, mid = 0;
+            while (left <= right)
+            {
+                mid = (left + right) >> 1;
+                if (A[mid] == target)
+                {
+                    idx = mid;
+                    break;
+                }
+                else if (A[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+        }
+        else
+        {
+            int mid = (l + r) >> 1;
+            if (A[mid] == target)
+            {
+                idx = mid;
+            }
+            else
+            {
+                idx = find(A, l, mid - 1, target);
+                idx = idx == -1 ? find(A, mid + 1, r, target) : idx;
+            }
+        }
+        return idx;
+    }
+
+public:
+    int search(vector<int> &A, int target)
+    {
+        int n = A.size();
+        return find(A,0,n-1,target);
+    }
+};
+
+int main()
+{
+    Solution s;
+    int A[]= { 3,1 };
+    vector<int> arr (begin(A),end(A));
+    cout << s.search(arr, 3) << endl;
+    return 0;
+}

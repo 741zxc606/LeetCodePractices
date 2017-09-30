8b491b4e7e938ed226a0b50391983c341887dff8
TesLA_422 <tesla422zxc@gmail.com>
2017-09-30 12:25:07 +0800

    Create 53.MaximumSubarray.cpp

---
+++ Algorithm/cpp/53.MaximumSubarray.cpp
@@ -0,0 +1,38 @@
+/*
+ * 53.Maximum Subarray
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * For example,given the array [-2,1,-3,4,-1,2,1,-5,4],
+ * the configuous subarray [4,-1,2,1] has the largest sum = 6.
+ *
+ * More practice:
+ * If you have figured out the O(n) solution,try coding another solution using the divide and conquer approach,
+ * which is more subtle.
+ */
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubuArray(vector<int>& nums)
+    {
+        int sum = 0, minSum = 0, maxSum = INT_MIN;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            maxSum = max(maxSum, sum - minSum);
+            minSum = min(minSum, sum);
+        }
+        return maxSum;
+    }
+};
+
+int main()
+{
+    vector<int> A{ -2,1,-3,4,-1,2,1,-5,4 };
+    Solution s;
+    cout << s.maxSubuArray(A) << endl;
+    return 0;
+}

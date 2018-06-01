b6d2f8bd391272c478abfbd00f77686f5544374e
TesLA_422 <tesla422zxc@gmail.com>
2018-06-01 14:43:18 +0800

    Create 208.ImplementTrie(PrefixTree).cpp

---
+++ Algorithm/cpp/208.ImplementTrie(PrefixTree).cpp
@@ -0,0 +1,79 @@
+/*
+ * 208. Implement Trie (Prefix Tree)
+ * Example: 
+ *      Trie trie = new Trie();
+ *      
+ *      trie.insert("apple");
+ *      trie.search("apple");   // returns true
+ *      trie.search("app");     // returns false
+ *      trie.startsWith("app"); // returns true
+ *      trie.insert("app"); 
+ *      trie.search("app");     // returns true
+ * Note: 
+ *      1. You may assume that all inputs are consist of lowercase letters a-z.
+ *      2. All inputs are guaranteed to be non-empty strings.
+ */
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class TrieNode {
+public:
+    //Initialize your data structure here.
+    TrieNode() :isWord(false) { }
+    unordered_map<char, TrieNode*>children;
+    bool isWord;
+};
+
+class Trie {
+public:
+    Trie() {
+        root = new TrieNode;
+    }
+
+    // Insert a word into the trie 
+    void insert(string s) {
+        if (s.size() <= 0) return;
+        TrieNode *node = root;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (node->children.find(s[i]) == node->children.end())
+            {
+                node->children[s[i]] = new TrieNode();
+            }
+            node = node->children[s[i]];
+        }
+        node->isWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string key){
+        return retrieve(key, true);
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix
+    bool startsWith(string prefix){
+        return retrieve(prefix, false);
+    }
+
+private:
+
+    inline bool retrieve(const string& key, bool isWord)
+    {
+        if (key.size() <= 0) return false;
+        TrieNode *node = root;
+        for (int i = 0; i < key.length(); i++)
+        {
+            if (node->children.find(key[i]) == node->children.end())
+            {
+                return false;
+            }
+            node = node->children[key[i]];
+        }
+        return isWord ? node->isWord : true;
+    }
+    TrieNode* root;
+};
